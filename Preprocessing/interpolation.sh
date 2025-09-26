3cd2e681b50ab1498de53ac8c5f02068a016a05c
mcannistra <95096730+Cannistra94@users.noreply.github.com>
2025-09-26 15:36:25 -0400

    Update interpolation.sh

--- Preprocessing/interpolation.sh
+++ Preprocessing/interpolation.sh
@@ -8,9 +8,9 @@ dos2unix $list
 
 for filename in `cat $list`; do
 
-ResampleImage 3 n4_corrected/${filename}_PET.nii ${filename}_PET.nii 1x1x1 3
+ResampleImage 3 n4_corrected/${filename}_T1.nii ${filename}_T1.nii 1x1x1 3
 
-echo "Resampled n4_corrected/${filename}_PET.nii to resampled/${filename}_T1.nii with 1x1x1 spacing using BSpline interpolation"
+echo "Resampled n4_corrected/${filename}_T1.nii to resampled/${filename}_T1.nii with 1x1x1 spacing using BSpline interpolation"
 
  
 
